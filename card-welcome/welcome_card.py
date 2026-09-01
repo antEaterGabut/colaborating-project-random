4940a81d64b30024dd5ad250c78e63b942341d98
LEO2581-B <raihansodikin2581@gmail.com>
2026-09-01 15:13:06 +0700

    pesan bebas

--- card-welcome/welcome_card.py
+++ card-welcome/welcome_card.py
@@ -1,4 +1,8 @@
 # block maneh han, kerjakeun we bagean inputna
-
+nama = input ("Masukan nama pengguna: " )
+umur = input (f"Berapa umurmu {nama}: " )
+hobi = input (f"Apa {nama} mempunyai hobi: ")
+status = input ("Apa status sekarang: ")
+pekerjaan = input ("Apa pekerjaanmu: ")
 
 #abi ngerjakeun bagean condisionalna
